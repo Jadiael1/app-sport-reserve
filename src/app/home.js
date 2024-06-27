f3199db9040148a081236be031399c326f9c7387
Wilksonflor <wilksonflor12@gmail.com>
2024-06-27 17:27:03 -0300

    feat(home): add user verification and logout functionality

--- src/app/home.js
+++ src/app/home.js
@@ -1,15 +1,77 @@
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { api_url } from "../constants/constants";
+import Validation from "./validation";
+import { router } from "expo-router";
 
-export default function home() {
-  const logout = () => {};
+export default function Home() {
+  const [user, setUser] = useState(null);
+  const [emailValid, setEmailValid] = useState(false);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const checkUserVerification = async () => {
+      try {
+        const storedToken = await AsyncStorage.getItem("TOKEN");
+        if (!storedToken) {
+          router.navigate("/");
+          return;
+        }
+        setToken(storedToken);
+
+        const response = await fetch(`${api_url}/auth/user`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        });
+
+        // if (!response.ok) {
+        //   router.navigate("/");
+        //   return;
+        // }
+
+        const userData = await response.json();
+        setUser(userData);
+
+        // Verifica se o e-mail foi verificado
+        if (userData && userData.email_verified_at === null) {
+          setEmailValid(true);
+        } else {
+          setEmailValid(false);
+        }
+      } catch (error) {
+        console.error("Error checking user verification:", error);
+      }
+    };
+
+    checkUserVerification();
+  }, []);
+
+  const logout = async () => {
+    try {
+      await AsyncStorage.removeItem("TOKEN");
+      await AsyncStorage.removeItem("EMAIL");
+      await AsyncStorage.removeItem("EMAIL_VERIFIED_AT");
+      router.navigate("/");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>home</Text>
-      <Pressable>
-        <Text onPress={logout}>Sair</Text>
-      </Pressable>
+      {emailValid ? (
+        <>
+          <Text>Home</Text>
+          <Pressable onPress={logout}>
+            <Text>Sair</Text>
+          </Pressable>
+        </>
+      ) : (
+        user && <Validation token={token} email={user.email} />
+      )}
     </View>
   );
 }
