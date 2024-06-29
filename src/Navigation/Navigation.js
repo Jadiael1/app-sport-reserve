b9716f909a55cafdc9aecf6d778a142701df7962
Wilksonflor <wilksonflor12@gmail.com>
2024-06-29 19:33:13 -0300

    feat(navigation): add new screens to navigation setup

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import TabNavigator from "./TabNavigator";
-import VerificationScreen from "../screens/VerificationScreen";
 import * as Linking from "expo-linking";
 import TabNavigator from "./TabNavigation";
+import LoginScreen from "../app/index";
+import AgendarHorario from "../app/agendarHorario";
+import HorariosAgendados from "../app/horariosAgendados";
+
 const Stack = createStackNavigator();
 
 const prefix = Linking.createURL("/");
@@ -13,24 +14,40 @@ const linking = {
   prefixes: [prefix],
   config: {
     screens: {
-      Home: "home",
-      Login: "login",
-      Verification: {
-        path: "auth/email/verify",
-        parse: {
-          id: (id) => `${id}`,
-          expires: (expires) => `${expires}`,
-          signature: (signature) => `${signature}`,
-        },
-      },
+      home: "home",
+      login: "index",
+      agendarHorario: "agendarHorario",
+      horariosAgendados: "horariosAgendados",
+      recoveryPassword: "recoveryPassword",
+      signup: "signup",
+      validation: "validation",
     },
   },
 };
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer linking={linking}>
-      <TabNavigator />
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="index">
+      <Stack.Screen
+        name="index"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="home"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="agendarHorario"
+        component={AgendarHorario}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="horariosAgendados"
+        component={HorariosAgendados}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
 }
