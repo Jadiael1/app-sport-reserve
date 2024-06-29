7d7d7a6112702196e9f6fa6c570eb7f553f91cfd
Wilksonflor <wilksonflor12@gmail.com>
2024-06-29 19:33:13 -0300

    feat(navigation): add new screens to navigation setup

--- src/Navigation/TabNavigation.js
+++ src/Navigation/TabNavigation.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../app/home";
+import AgendarHorario from "../app/agendarHorario";
+import HorariosAgendados from "../app/horariosAgendados";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="home" component={Home} />
-      {/* Adicione mais telas conforme necessário */}
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="AgendarHorario"
+        component={AgendarHorario}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="HorariosAgendados"
+        component={HorariosAgendados}
+        options={{ headerShown: false }}
+      />
     </Tab.Navigator>
   );
 }
