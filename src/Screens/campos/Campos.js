ac22cb8a34fe7e97ae1c154c538b3bccc58c8c2f
Wilksonflor <wilksonflor12@gmail.com>
2024-07-01 20:58:54 -0300

    feat(campos): add modal for adding fields with auth token

--- src/Screens/campos/Campos.js
+++ src/Screens/campos/Campos.js
@@ -7,13 +7,15 @@ import {
   FlatList,
   StyleSheet,
   Alert,
+  Modal,
 } from "react-native";
 import axios from "axios";
 import { ValorHora } from "../../components/Inputs/ValorHora";
 import { api_url } from "../../constants/constants";
 import { AntDesign } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Campos = () => {
   const [fields, setFields] = useState([]);
@@ -22,6 +24,7 @@ const Campos = () => {
   const [type, setType] = useState("");
   const [hourlyRate, setHourlyRate] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     fetchFields();
@@ -56,14 +59,24 @@ const Campos = () => {
     }
 
     try {
-      const hourlyRateNumber = parseFloat(hourlyRate.replace(",", "."));
-
-      const response = await axios.post(`${api_url}/fields`, {
-        name,
-        location,
-        type,
-        hourly_rate: hourlyRateNumber,
-      });
+      const hourlyRateNumber = parseFloat(
+        hourlyRate.replace(",", ".").replace("R$", "")
+      );
+      const token = await AsyncStorage.getItem("TOKEN");
+      const response = await axios.post(
+        `${api_url}/fields`,
+        {
+          name,
+          location,
+          type,
+          hourly_rate: hourlyRateNumber,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       if (response.data.status === "success") {
         setFields([...fields, response.data.data]);
@@ -72,6 +85,7 @@ const Campos = () => {
         setLocation("");
         setType("");
         setHourlyRate("");
+        setModalOpen(false);
         Alert.alert("Sucesso", "Campo adicionado com sucesso.");
       } else {
         Alert.alert("Erro", "Não foi possível adicionar o campo.");
@@ -92,8 +106,14 @@ const Campos = () => {
     console.log("deletar campo");
   };
 
+  const handleSetHourlyRate = () => {
+    setHourlyRate;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Lista de Campos</Text>
+
       <View style={styles.searchContainer}>
         <TextInput
           placeholder="Localizar campo..."
@@ -106,77 +126,94 @@ const Campos = () => {
           color="black"
           style={styles.searchIcon}
         />
+        <Pressable style={styles.addButton} onPress={() => setModalOpen(true)}>
+          <Text style={styles.addButtonText}>Adicionar campo</Text>
+        </Pressable>
       </View>
-      <View>
-        <Text style={styles.title}>Campos</Text>
-        <FlatList
-          style={styles.list}
-          data={filteredFields}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <View style={styles.fieldItem}>
-              <View style={styles.fieldTextContainer}>
-                <Text style={styles.fieldText}>
-                  Nome: <Text style={styles.descriptionItens}>{item.name}</Text>
-                </Text>
-                <Text style={styles.fieldText}>
-                  Localização:{" "}
-                  <Text style={styles.descriptionItens}>{item.location}</Text>
-                </Text>
-                <Text style={styles.fieldText}>
-                  Modalidade:{" "}
-                  <Text style={styles.descriptionItens}>{item.type}</Text>
-                </Text>
-                <Text style={styles.fieldText}>
-                  Valor da Hora:{" "}
-                  <Text style={styles.descriptionItens}>
-                    {item.hourly_rate}
-                  </Text>
-                </Text>
-              </View>
-              <View style={styles.iconContainer}>
-                <Pressable style={styles.iconButton} onPress={updateFields}>
-                  <AntDesign name="edit" size={24} color="blue" />
-                </Pressable>
-                <Pressable style={styles.iconButton} onPress={deleteField}>
-                  <AntDesign name="delete" size={24} color="red" />
-                </Pressable>
-              </View>
-            </View>
-          )}
-        />
 
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Nome do campo"
-            value={name}
-            onChangeText={setName}
-            style={styles.input}
-          />
-          <TextInput
-            placeholder="Localização"
-            value={location}
-            onChangeText={setLocation}
-            style={styles.input}
-          />
-          <TextInput
-            placeholder="Modalidade do campo"
-            value={type}
-            onChangeText={setType}
-            style={styles.input}
-          />
-          <ValorHora
-            placeholder="Valor da hora"
-            value={hourlyRate}
-            onChangeText={setHourlyRate}
-            style={styles.input}
-            keyboardType="numeric"
-          />
-          <Pressable style={styles.button} onPress={addField}>
-            <Text style={styles.buttonText}>Adicionar Campo</Text>
-          </Pressable>
+      <FlatList
+        style={styles.list}
+        data={filteredFields}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.fieldItem}>
+            <View style={styles.fieldTextContainer}>
+              <Text style={styles.fieldText}>
+                Nome: <Text style={styles.descriptionItens}>{item.name}</Text>
+              </Text>
+              <Text style={styles.fieldText}>
+                Localização:{" "}
+                <Text style={styles.descriptionItens}>{item.location}</Text>
+              </Text>
+              <Text style={styles.fieldText}>
+                Modalidade:{" "}
+                <Text style={styles.descriptionItens}>{item.type}</Text>
+              </Text>
+              <Text style={styles.fieldText}>
+                Valor da Hora:{" "}
+                <Text style={styles.descriptionItens}>
+                  R$ {item.hourly_rate}
+                </Text>
+              </Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Pressable style={styles.iconButton} onPress={updateFields}>
+                <AntDesign name="edit" size={24} color="blue" />
+              </Pressable>
+              <Pressable style={styles.iconButton} onPress={deleteField}>
+                <AntDesign name="delete" size={24} color="red" />
+              </Pressable>
+            </View>
+          </View>
+        )}
+      />
+
+      <Modal
+        visible={modalOpen}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setModalOpen(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Adicionar Campo</Text>
+            <TextInput
+              placeholder="Nome do campo"
+              value={name}
+              onChangeText={setName}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Localização"
+              value={location}
+              onChangeText={setLocation}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Modalidade do campo"
+              value={type}
+              onChangeText={setType}
+              style={styles.input}
+            />
+            <ValorHora
+              placeholder="Valor da hora"
+              value={hourlyRate}
+              onChangeText={setHourlyRate}
+              style={styles.input}
+              keyboardType="numeric"
+            />
+            <Pressable style={styles.button} onPress={addField}>
+              <Text style={styles.buttonText}>Adicionar Campo</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalOpen(false)}
+            >
+              <Text style={styles.buttonText}>Cancelar</Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -187,30 +224,47 @@ const styles = StyleSheet.create({
     backgroundColor: "#E8F4F8",
     padding: 16,
   },
+
   searchContainer: {
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "space-between",
     backgroundColor: "#fff",
     borderRadius: 8,
     paddingVertical: 8,
     paddingHorizontal: 16,
     marginBottom: 16,
   },
+
   searchInput: {
-    flex: 1,
+    // flex: 1,
     fontSize: 16,
     padding: 8,
   },
+
   searchIcon: {
     marginLeft: 8,
   },
+
+  addButton: {
+    backgroundColor: "#3D5A80",
+    padding: 10,
+    borderRadius: 8,
+  },
+
+  addButtonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+
   title: {
     fontSize: 24,
     fontWeight: "bold",
     color: "#3D5A80",
-    marginBottom: 16,
+    marginVertical: 20,
     textAlign: "center",
   },
+
   fieldItem: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -243,26 +297,52 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginTop: 16,
   },
+  modal: {
+    backgroundColor: "red",
+  },
   input: {
     backgroundColor: "#fff",
     padding: 12,
     borderRadius: 8,
-    marginVertical: 8,
+    marginVertical: 10,
     fontSize: 16,
     elevation: 5,
   },
   button: {
-    backgroundColor: "#3D5A80",
+    backgroundColor: "green",
     paddingVertical: 16,
     borderRadius: 8,
     alignItems: "center",
     marginTop: 16,
     elevation: 5,
   },
+  buttonClose: {
+    backgroundColor: "#c44d4d",
+  },
   buttonText: {
     color: "#fff",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#fff",
+
+    borderRadius: 8,
+    padding: 16,
+    elevation: 10,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 16,
+    textAlign: "center",
   },
 });
 
