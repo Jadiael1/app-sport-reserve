07f21e0fb0020406b25ed743e2f72c9839fca16b
Jadiael1 <jadiael@hotmail.com.br>
2024-07-07 14:21:32 -0300

    feat: Add logging to startExpo.js

--- deploy.sh
+++ deploy.sh
@@ -19,26 +19,37 @@ chmod a+x ./node_modules/.bin/expo
 # Cria startExpo.js
 cat << 'EOF' > startExpo.js
 const { exec } = require('child_process');
+const fs = require('fs');
+
+// Cria um stream de escrita para o arquivo de log
+const logStream = fs.createWriteStream('expo_start.log', { flags: 'a' });
 
 // Executa o comando 'expo start --web'
 const process = exec('./node_modules/.bin/expo start --web --port 50003', (error, stdout, stderr) => {
   if (error) {
-    console.error(`Erro ao executar o comando: ${error}`);
+    const errorMsg = `Erro ao executar o comando: ${error}\n`;
+    console.error(errorMsg);
+    logStream.write(errorMsg);
     return;
   }
   if (stderr) {
-    console.error(`Erro: ${stderr}`);
+    const errorOutput = `Erro: ${stderr}\n`;
+    console.error(errorOutput);
+    logStream.write(errorOutput);
     return;
   }
   console.log(`Saída: ${stdout}`);
+  logStream.write(`Saída: ${stdout}\n`);
 });
 
 // Garante que a saída do processo seja exibida no console
 process.stdout.on('data', (data) => {
   console.log(data.toString());
+  logStream.write(data.toString());
 });
 
 process.stderr.on('data', (data) => {
   console.error(data.toString());
+  logStream.write(data.toString());
 });
 EOF
