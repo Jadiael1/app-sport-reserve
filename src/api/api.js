2aff2310ec339de2fee9e3adaaf872857a396741
Wilksonflor <wilksonflor12@gmail.com>
2024-07-23 19:32:06 -0300

    style(api.js): Remove extra whitespace in fetchFields function

--- src/api/api.js
+++ src/api/api.js
@@ -33,9 +33,11 @@ export const fetchUser = async (setUser) => {
 export const fetchFields = async () => {
   try {
     const response = await axios.get(`${api_url}/fields`);
+   
 
     if (response.data.status === "success") {
       return response.data.data.data;
+      
     } else {
       throw new Error("Erro ao buscar campos");
     }
