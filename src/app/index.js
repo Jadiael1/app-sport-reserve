abf7bee4d92e1f3abc2ca625acaa23652d7ffb28
Wilksonflor <wilksonflor12@gmail.com>
2024-08-06 16:43:17 -0300

    refactor(Login): Use context for login handling

--- src/app/index.js
+++ src/app/index.js
@@ -19,11 +19,15 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { api_url } from "../constants/constants";
 import { CustomPasswordInput } from "../components/Inputs/CustomInputPassword";
+import { useSession } from "../context/UserContext";
 
 export default function Login() {
+  const { signIn, error } = useSession();
   const navigation = useNavigation();
   const [loginError, setLoginError] = useState("");
   const [loadingLogin, setLoadingLogin] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const {
     control,
     handleSubmit,
@@ -38,49 +42,53 @@ export default function Login() {
   });
 
   const handleLogin = async ({ email, password }) => {
-    setLoadingLogin(true);
+    // setLoadingLogin(true);
 
-    if (!isValidEmail(email)) {
-      setError("email", {
-        type: "manual",
-        message: "Por favor, insira um e-mail válido",
-      });
-      Vibration.vibrate(400);
-      setLoadingLogin(false);
-      return;
-    }
+    // if (!isValidEmail(email)) {
+    //   setError("email", {
+    //     type: "manual",
+    //     message: "Por favor, insira um e-mail válido",
+    //   });
+    //   Vibration.vibrate(400);
+    //   setLoadingLogin(false);
+    //   return;
+    // }
 
-    if (!isValidPassword(password)) {
-      setError("password", {
-        type: "manual",
-        message: "Insira a senha",
-      });
-      Vibration.vibrate(400);
-      setLoadingLogin(false);
-      return;
-    }
+    // if (!isValidPassword(password)) {
+    //   setError("password", {
+    //     type: "manual",
+    //     message: "Insira a senha",
+    //   });
+    //   Vibration.vibrate(400);
+    //   setLoadingLogin(false);
+    //   return;
+    // }
 
-    try {
-      const response = await axios.post(`${api_url}/auth/signin`, {
-        email,
-        password,
-      });
+    // try {
+    //   const response = await axios.post(`${api_url}/auth/signin`, {
+    //     email,
+    //     password,
+    //   });
 
-      const { token, user } = response.data.data;
-      console.log("resposta do login", response.data);
-      await AsyncStorage.setItem("TOKEN", token);
-      await AsyncStorage.setItem("EMAIL", user.email);
-      await AsyncStorage.setItem("EMAIL_VERIFIED_AT", user.email_verified_at);
-      await AsyncStorage.setItem("IS_ADMIN", JSON.stringify(user.is_admin));
+    //   const { token, user } = response.data.data;
+    //   console.log("resposta do login", response.data);
+    //   await AsyncStorage.setItem("TOKEN", token);
+    //   await AsyncStorage.setItem("EMAIL", user.email);
+    //   await AsyncStorage.setItem("EMAIL_VERIFIED_AT", user.email_verified_at);
+    //   await AsyncStorage.setItem("IS_ADMIN", JSON.stringify(user.is_admin));
 
-      navigation.navigate("home");
-      reset();
-    } catch (error) {
-      setLoginError("Verifique suas credenciais, por favor!");
-      Vibration.vibrate(400);
-    } finally {
-      setLoadingLogin(false);
-    }
+    //   navigation.navigate("home");
+    //   reset();
+    // } catch (error) {
+    //   setLoginError("Verifique suas credenciais, por favor!");
+    //   Vibration.vibrate(400);
+    // } finally {
+    //   setLoadingLogin(false);
+    // }
+
+    // Login usando useContext
+    await signIn(email, password);
+    navigation.navigate("home");
   };
 
   const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
